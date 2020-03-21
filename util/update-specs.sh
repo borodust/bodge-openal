c353024a1480fef7286b536ddc3abd5ae857a598
Pavel Korolev <dev@borodust.org>
2020-03-21 18:56:40 +0300

    Unbodge library

--- util/update-specs.sh
+++ util/update-specs.sh
@@ -2,6 +2,6 @@
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-lisp download-specs --lib-postfix ".bodged" \
+lisp download-specs \
      openal https://github.com/borodust/bodge-openal \
      "$script_dir/../src/spec/" $1
